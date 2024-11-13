d3a08246ad68a942e9f83fe08d37ef2e152cd40b
Arlo Adams <arlo.s.adams@gmail.com>
2024-11-13 16:02:35 -0700

    Updated vite

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/raspberry-pi-kiosk/",
   plugins: [
     vue(),
     vueDevTools(),
